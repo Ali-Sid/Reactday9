529cd1c01458487bccff3d59aa969e898af1466f
Ali-Sid <59683051+Ali-Sid@users.noreply.github.com>
2023-05-28 18:29:54 +0530

    upload files

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <h1>30 Days Of React Challenge</h1>
+    <h2>Day 9: Conditional Rendering</h2>
+    </>
+  )
+}
+
+export default App
---
+++ src/exercise.js
@@ -0,0 +1,16 @@
+// 30-days-of-react-challenge: Day9: Conditional Rendering
+
+// Exercises: Level 1
+
+// 1. What is conditional rendering?
+// It is a way to render different JSX or component at different condition.
+
+// 2. How do you implement conditional rendering?
+// We can impplement conditional rendering by using if-else statements or ternary operators or &&.
+
+// 3. Which method of conditional rendering do you prefer to use?
+// I'd prefer ternary operator as we can write faster than if else statements, it takes less space than regular if-else, and we can also use ternary operator inside styles, className or many places in a component than regular if else statement.
+
+
+
+// Exercises: Level 2
